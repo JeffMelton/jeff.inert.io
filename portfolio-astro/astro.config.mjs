9d6c4daa95d638aeec6956e621275eac8277931c
Jeff Melton <jeff@melton.cloud>
2025-08-16 19:48:23 -0500

    moves to Tailwind for better responsive design

--- portfolio-astro/astro.config.mjs
+++ portfolio-astro/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'static',
   adapter: cloudflare(),
-  site: 'https://jeff.inert.io'
+  site: 'https://jeff.inert.io',
+  integrations: [tailwind()]
 });
